d5504fa7f65e34624c45f36b999dbf374ad999b0
tjdanqkr <tjdanqkr@gmail.com>
2023-08-11 12:08:50 +0900

    sss

--- test6/src/components/signup/Signup.jsx
+++ test6/src/components/signup/Signup.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 
 const Signup = () => {
     const users = [{ id: '1q2w3e' }, { id: '1234' }, { id: 'park' }]
-    const [id, setId] = useState();
-    const onChangeId = e => setId(e.target.value)
+    const [id, setId] = useState({ id: "", exist: false });
+    const onChangeId = e => setId((prev) =>
+        ({ ...prev, id: e.target.value }))
+    const checkId = () => setId((prev) =>
+        ({ ...prev, exist: users.find(u => u.id === prev.id) }))
     return <div style={{
         display: 'flex',
         flexDirection: 'column',
@@ -14,10 +17,16 @@ const Signup = () => {
             name="id"
             placeholder="id"
             onChange={onChangeId}
-            onBlur={() => console.log('asda')}
+            onBlur={checkId}
         />
-        <input name="password" type="password" placeholder="password"></input>
-        <input name="repassword" type="password" placeholder="repassword"></input>
+        {id.exist &&
+            <p style={{ color: 'red' }}>있는 아이디입니다</p>}
+        <input name="password"
+            type="password"
+            placeholder="password" />
+        <input name="repassword"
+            type="password"
+            placeholder="repassword" />
     </div>
 }
 export default Signup;
