208555c9e5d6c9d7170b273c30a989ba5690e491
tjdanqkr <tjdanqkr@gmail.com>
2023-08-11 12:30:03 +0900

    s

--- test6/src/components/signup/Signup.jsx
+++ test6/src/components/signup/Signup.jsx
@@ -7,6 +7,22 @@ const Signup = () => {
         ({ ...prev, id: e.target.value }))
     const checkId = () => setId((prev) =>
         ({ ...prev, exist: users.find(u => u.id === prev.id) }))
+    const [password, setPassword] = useState({
+        password: "", repassword: "", isEqual: true
+    })
+
+    const onChangePassword = (e) => {
+        const { name, value } = e.target
+        setPassword((prev) => ({ ...prev, [name]: value }))
+    }
+    const passwordCheck = () => {
+        setPassword((prev) => ({
+            ...prev
+            , isEqual: prev.password === prev.repassword
+        }))
+    }
+
+
     return <div style={{
         display: 'flex',
         flexDirection: 'column',
@@ -23,10 +39,17 @@ const Signup = () => {
             <p style={{ color: 'red' }}>있는 아이디입니다</p>}
         <input name="password"
             type="password"
-            placeholder="password" />
+            placeholder="password"
+            onChange={onChangePassword}
+        />
         <input name="repassword"
             type="password"
-            placeholder="repassword" />
+            placeholder="repassword"
+            onChange={onChangePassword}
+            onBlur={passwordCheck}
+        />
+        {password.isEqual || <p style={{ color: 'red' }}>
+            비밀번호가 다릅니다.</p>}
     </div>
 }
 export default Signup;
