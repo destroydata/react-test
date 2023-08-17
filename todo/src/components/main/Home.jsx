109fe836ca0d04a3b03030e666ff4b5f8e1d515e
tjdanqkr <tjdanqkr@gmail.com>
2023-08-17 15:01:09 +0900

    set

--- todo/src/components/main/Home.jsx
+++ todo/src/components/main/Home.jsx
@@ -7,21 +7,28 @@ const Home = () => {
     const [all, setAll] = useState([]);
     const [message, setMessage] = useState("");
     const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(5);
+    const [totalPages, setTotalPages] = useState(0);
     const getAll = async () => {
         setMessage("")
         setLoading(true)
         try {
-            const data = await api("/api/v1/todos", "GET")
+            const data = await api(`/api/v1/todos` +
+                `?page=${page}&size=${size}`, "GET")
             setLoading(false)
-            setMessage('error.response.data')
             setAll(data.content)
+            setTotalPages(data.totalPages)
         } catch (error) {
             setMessage(error.response.data)
         }
     }
     useEffect(() => {
         getAll();
-    }, [])
+    }, [page])
+    const changePage = (i) => {
+        setPage(i)
+    }
     return <div>
         {loading && <Loading />}
         {message && <Toast message={message} />}
@@ -32,13 +39,18 @@ const Home = () => {
                 <th>check</th>
             </thead>
             <tbody>
-                {all.map(data => <tr>
+                {all.map(data => <tr key={data.id}>
                     <td>{data.content}</td>
                     <td>{data.user.name}</td>
                     <td><input type="checkbox" readOnly checked={data.isCompleted} /></td>
                 </tr>)}
             </tbody>
         </table>}
+        <div style={{ display: "flex" }}>
+            {Array(totalPages).fill(0).map((el, i) =>
+                <button onClick={() => changePage(i)}>{i + 1}</button>
+            )}
+        </div>
     </div>
 }
 
