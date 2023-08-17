993472f54e0d2e81f03fd5209602f624ee548723
tjdanqkr <tjdanqkr@gmail.com>
2023-08-17 14:08:56 +0900

    Sdf

--- todo/src/components/main/Home.jsx
+++ todo/src/components/main/Home.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { api } from "../../network/api";
+import Loading from "../tools/Loading";
+import Toast from "../tools/Toast";
+
 const Home = () => {
-    return <h1>대충 홈</h1>
+    const [all, setAll] = useState([]);
+    const [message, setMessage] = useState("");
+    const [loading, setLoading] = useState(false);
+    const getAll = async () => {
+        setMessage("")
+        setLoading(true)
+        try {
+            const data = await api("/api/v1/todos", "GET")
+            setLoading(false)
+            setMessage('error.response.data')
+            setAll(data.content)
+        } catch (error) {
+            setMessage(error.response.data)
+        }
+    }
+    useEffect(() => {
+        getAll();
+    }, [])
+    return <div>
+        {loading && <Loading />}
+        {message && <Toast message={message} />}
+        {(loading || message) || <table>
+            <thead>
+                <th>content</th>
+                <th>name</th>
+                <th>check</th>
+            </thead>
+            <tbody>
+                {all.map(data => <tr>
+                    <td>{data.content}</td>
+                    <td>{data.user.name}</td>
+                    <td><input type="checkbox" readOnly checked={data.isCompleted} /></td>
+                </tr>)}
+            </tbody>
+        </table>}
+    </div>
 }
 
 export default Home;
