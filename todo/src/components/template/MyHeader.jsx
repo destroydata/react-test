df01ec0ecd01f5c63876fbd56f8e5d824e624b3d
tjdanqkr <tjdanqkr@gmail.com>
2023-08-18 12:41:15 +0900

    fds

--- todo/src/components/template/MyHeader.jsx
+++ todo/src/components/template/MyHeader.jsx
@@ -1,28 +1,32 @@
 import { Link, useNavigate } from "react-router-dom";
 import { api } from "../../network/api";
 import { useEffect } from "react";
-
-const MyHeader = ({ me, setMe }) => {
+import { setMe, test } from "../../feature/me/meSlice";
+import { useDispatch, useSelector } from 'react-redux'
+const MyHeader = () => {
     const nav = useNavigate();
+    const dispatch = useDispatch()
+    const me = useSelector(state => state.me)
     const logout = () => {
         localStorage.clear()
-        setMe({
+        dispatch(setMe({
             id: "",
             money: 0,
             name: "",
             token: ""
-        });
+        }));
         nav('/login')
     }
     const getMe = async () => {
         try {
             const data = await api('/api/v1/users/me', 'GET')
-            setMe(data)
+            dispatch(setMe(data))
             localStorage.setItem("token", data.token)
         } catch (error) {
             nav('/login')
         }
     }
+    const setTest = () => dispatch(test())
 
     useEffect(() => {
         getMe()
@@ -34,6 +38,7 @@ const MyHeader = ({ me, setMe }) => {
         <p>잔고 : {me.money}</p>
         <Link to='/todo/create'>+</Link>
         <button onClick={logout}>로그아웃</button>
+        <button onClick={setTest}>test</button>
     </header>
 }
 
--- todo/src/feature/me/meSlice.js
+++ todo/src/feature/me/meSlice.js
@@ -10,15 +10,21 @@ export const meSlice = createSlice({
     name: "me",
     initialState: { ...IME },
     reducers: {
-        setMe: state => {
+        test: state => {
             state.id = "1";
             state.money = 10000;
             state.name = "김수동";
             state.token = "token";
+        },
+        setMe: (state, { payload }) => {
+            state.id = payload.id
+            state.money = payload.money
+            state.name = payload.name
+            state.token = payload.token
         }
     }
 })
 
-export const { setMe } = meSlice.actions
+export const { test, setMe } = meSlice.actions
 
 export default meSlice.reducer
